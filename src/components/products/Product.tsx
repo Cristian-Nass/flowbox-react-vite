f4b55122b2687edcb1c5f5a88c7c9e48e562ea52
Cristian Nass <cristian.tirdad@gmail.com>
2023-04-30 20:54:45 +0200

    save

--- src/components/products/Product.tsx
+++ src/components/products/Product.tsx
@@ -1,14 +1,14 @@
 import {ProductsType} from '../../models/Types';
-import {ContainerCard, ContainerImg} from '../styles/ContainerCards';
+// import {ContainerCard, ContainerImg} from '../styles/ContainerCards';
 
 const Product = (props: ProductsType) => {
   return (
-    <ContainerCard color="list">
+    <div color="list">
       <div className="card-details-image">
-        <ContainerImg color="list" src={props.image} />
+        <img color="list" src={props.image} />
       </div>
-      <div className="card-details-text">{props.title}</div>
-    </ContainerCard>
+      {/* <div className="card-details-text">{props.title}</div> */}
+    </div>
   );
 };
 
