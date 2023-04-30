ae16842a5dd0ed86e4f79d6eec09d9b91bd64984
Cristian Nass <cristian.tirdad@gmail.com>
2023-04-30 13:57:24 +0200

    Update Url

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,6 +1,6 @@
 export const getProducts = async () => {
   const response = await fetch(
-    'https://gist.githubusercontent.com/Cristian-Nass/f5e2dedf7f090591fca4ba9d9873a2f9/raw/0053964fc901421c6f6fa62deb035a220764c958/products.json',
+    'https://gist.githubusercontent.com/Cristian-Nass/f5e2dedf7f090591fca4ba9d9873a2f9/raw/29ac2ebc24324ec644cf4c1a69a1e50f0208f73e/products.json',
     {}
   );
 
