b4d1e6312da6f0e1900c0009dc91f2502bdcf91c
Cristian Nass <cristian.tirdad@gmail.com>
2023-04-30 21:07:05 +0200

    add style to store

--- src/shareds/StyleSelector.tsx
+++ src/shareds/StyleSelector.tsx
@@ -1,18 +1,31 @@
+import {useStore} from '../store';
+
 const StyleSelector = () => {
+  const {setStyle} = useStore();
+
+  const buttons = [
+    {title: 'List', value: 'list', id: '1'},
+    {title: 'Grid', value: 'grid', id: '2'},
+    {title: 'Slide', value: 'slide', id: '3'},
+    {title: 'Card', value: 'card', id: '4'},
+  ];
+
+  const setStyleHandler = (styleSetter: string) => {
+    setStyle(styleSetter);
+  };
+
   return (
     <div className="btn-group">
-      <button type="button" className="btn btn-primary" style={{width: '60px'}}>
-        List
-      </button>
-      <button type="button" className="btn btn-primary" style={{width: '60px'}}>
-        Grid
-      </button>
-      <button type="button" className="btn btn-primary" style={{width: '60px'}}>
-        Slide
-      </button>
-      <button type="button" className="btn btn-primary" style={{width: '60px'}}>
-        Card
-      </button>
+      {buttons.map((button) => (
+        <button
+          key={button.id}
+          type="button"
+          className="btn btn-primary"
+          style={{width: '60px'}}
+          onClick={() => setStyleHandler(button.value)}>
+          {button.title}
+        </button>
+      ))}
     </div>
   );
 };
--- src/store/index.ts
+++ src/store/index.ts
@@ -6,15 +6,26 @@ import {ProductsType} from '../models/Types';
 export interface ProductsStore {
   products: ProductsType[];
   setProducts: (products: ProductsType[]) => void;
+  style: string;
+  setStyle: (style: string) => void;
 }
 
 export const initialState = {
   products: [],
+  style: 'grid',
 };
 
 export const useStore = create<ProductsStore, any>(
   devtools((set) => ({
     ...initialState,
+    setStyle: (style: string) =>
+      set(
+        produce((state: ProductsStore) => {
+          state.style = style;
+        }),
+        false,
+        'set Style'
+      ),
     setProducts: (products: ProductsType[]) =>
       set(
         produce((state: ProductsStore) => {
