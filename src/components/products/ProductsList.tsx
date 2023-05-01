19c7c7f2ca5ecb8ddbd5eda9112792867d172d50
Cristian Nass <cristian.tirdad@gmail.com>
2023-05-01 20:19:01 +0200

    Implemented ProductsSlideView

--- src/components/products/ProductsList.tsx
+++ src/components/products/ProductsList.tsx
@@ -5,6 +5,7 @@ import Product from './Product';
 import {ProductsType} from '../../models/Types';
 import {ContainerCards} from '../styles/ContainerCards';
 import {useMediaQuery} from 'usehooks-ts';
+import ProductsSlideView from './ProductsSlideView';
 
 const ProductsList = () => {
   const [isLoading, error] = useGetProducts();
@@ -15,7 +16,7 @@ const ProductsList = () => {
   const matches = useMediaQuery('(min-width: 724px)');
 
   const setDataForCard = (p: ProductsType[], index: number) => {
-    if (index <= 0) return [p[0], p[1], p[2]];
+    if (index <= 1) return [p[0], p[1], p[2]];
     if (index >= 10) return [p[10], p[11], p[12]];
     return [p[index], p[index + 1], p[index + 2]];
   };
@@ -56,34 +57,9 @@ const ProductsList = () => {
   if (error) {
     return <div>error</div>;
   }
-  if (style === 'slide')
-    return (
-      <div style={{display: 'flex', justifyContent: 'center'}}>
-        <button
-          type="button"
-          className="btn btn-primary defult-blue-color"
-          style={{width: '60px'}}
-          onClick={() => sliderCounter('down')}>
-          Prev
-        </button>
-        <Product
-          title={productsData[currentSlide]?.title}
-          image={productsData[currentSlide]?.image}
-          descriptions={productsData[currentSlide]?.descriptions}
-          id={productsData[currentSlide]?.id}
-          price={productsData[currentSlide]?.price}
-        />
-        <button
-          type="button"
-          className="btn btn-primary defult-blue-color"
-          style={{width: '60px'}}
-          onClick={() => sliderCounter('up')}>
-          Next
-        </button>
-      </div>
-    );
-
   const listData = style !== 'card' ? productsData : productsDataCard;
+
+  if (style === 'slide') return <ProductsSlideView />;
   return (
     <>
       <div style={{display: style === 'card' ? 'flex' : '', alignItems: 'center'}}>
---
+++ src/components/products/ProductsSlideView.tsx
@@ -0,0 +1,47 @@
+import {useState} from 'react';
+import {useStore} from '../../store';
+import Product from './Product';
+
+const ProductsSlideView = () => {
+  const {products} = useStore();
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const sliderCounter = (data: string) => {
+    if (data === 'up') {
+      if (currentSlide === products.length - 1) return;
+      setCurrentSlide(currentSlide + 1);
+    }
+    if (data === 'down') {
+      if (currentSlide === 0) return;
+      setCurrentSlide(currentSlide - 1);
+    }
+  };
+
+  return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <button
+        type="button"
+        className="btn btn-primary defult-blue-color"
+        style={{width: '60px'}}
+        onClick={() => sliderCounter('down')}>
+        Prev
+      </button>
+      <Product
+        title={products[currentSlide]?.title}
+        image={products[currentSlide]?.image}
+        descriptions={products[currentSlide]?.descriptions}
+        id={products[currentSlide]?.id}
+        price={products[currentSlide]?.price}
+      />
+      <button
+        type="button"
+        className="btn btn-primary defult-blue-color"
+        style={{width: '60px'}}
+        onClick={() => sliderCounter('up')}>
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default ProductsSlideView;
