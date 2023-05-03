a63d8fa8b34a504bc8d18ca22c5ba977868949df
Cristian Nass <cristian.tirdad@gmail.com>
2023-05-03 11:09:26 +0200

    save

--- src/components/products/__test__/Product.test.tsx
+++ src/components/products/__test__/Product.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Product from "../Product";
 
-it("Image tag exisating", async () => {
+it("props exisating", async () => {
   render(<Product title="" descriptions="" image="" id="" />);
   const imageElement = await screen.findByTitle("image");
   expect(imageElement).toBeInTheDocument();
